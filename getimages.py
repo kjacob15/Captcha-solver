358da7077f9132054f0c204243855dc8dfb645a9
kjacob15 <91503607+kjacob15@users.noreply.github.com>
2021-10-14 15:49:31 +0100

    Cleared the test folder

--- getimages.py
+++ getimages.py
@@ -3,21 +3,19 @@ import pandas as pd
 import wget
 
 df = pd.read_csv("kjacob-challenge-filenames.csv", header=None)
-names= np.array(df.iloc[:,0])
-play=[]
+names = np.array(df.iloc[:, 0])
 url = 'https://cs7ns1.scss.tcd.ie/2122/kjacob/pi-project2/'
 
-for i in range(10):
-  play.append(names[i])
+# play=[]
+# for i in range(10):
+#   play.append(names[i])
+#
+# print(len(play))
 
-print(len(play))
-
-
-
-# print('Beginning file download with wget module')
+print('Beginning file download with wget module')
 try:
-  for j in range(len(play)):
-    wget.download(url+play[j], 'test2/')
+    for j in range(len(names)):
+        wget.download(url + names[j], 'test/')
 
 except Exception as exc:
-  print('Exception details ' + str(exc) + ' Name of file= ' + play[i])
+    print('Exception details ' + str(exc) + ' Name of file= ' + names[j])
